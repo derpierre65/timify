086f0898823a01a0f8ff8ee487e8e62b865838b0
derpierre65 <git@derpierre65.de>
2025-06-16 00:07:52 +0100

    chore: init commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer
+      bordered
+    >
+      <q-list>
+        Drawer
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    Main Page
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+</script>
